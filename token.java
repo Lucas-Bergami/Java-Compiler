09392d71adf5888eb1a2c8fd1d155205d5ade0de
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-09-25 10:58:37 -0300

    added token class

---
+++ token.java
@@ -0,0 +1,53 @@
+public class token {
+    public enum Type {
+        FUNCTION,
+        MAIN,
+        LET,
+        INT,
+        FLOAT,
+        CHAR,
+        IF,
+        ELSE,
+        WHILE,
+        PRINTLN,
+        RETURN,
+        LBRACKET,
+        RBRACKET,
+        LBRACE,
+        RBRACE,
+        ARROW,
+        COLON,
+        SEMICOLON,
+        COMMA,
+        ASSIGN,
+        EQ,
+        NE,
+        GT,
+        GE,
+        LT,
+        LE,
+        PLUS,
+        MINUS,
+        MULT,
+        DIV,
+        ID,
+        INT_CONST,
+        FLOAT_CONST,
+        CHAR_LITERAL,
+        FMT_STRING
+    }
+
+    private Type type;
+    private String lexeme;
+    private int lineNumber;
+
+    public token(Type type, String lexeme, int lineNumber) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.lineNumber = lineNumber;
+    }
+
+    public Type getType() { return type; }
+    public String getLexeme() { return lexeme; }
+    public int getLineNumber() { return lineNumber; }
+}
