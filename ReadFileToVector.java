baea719f8f93ab6e19bed628ed34d5aeba33d163
lucas-bergami <lucasbergami5@gmail.com>
2025-09-25 12:19:05 -0300

    moved ReadFileToVector to its own class and remade main

---
+++ ReadFileToVector.java
@@ -0,0 +1,25 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+public class ReadFileToVector {
+  public static Vector<Character> readFileToVector(String fileName) {
+
+    Vector<Character> characters = new Vector<>();
+
+    try (FileReader reader = new FileReader(fileName)) {
+      int c;
+      while ((c = reader.read()) != -1) {
+        characters.add((char) c);
+      }
+    } catch (IOException e) {
+      System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+    }
+
+    System.out.println("Caracteres lidos do arquivo:");
+    for (Character ch : characters) {
+      System.out.print(ch);
+    }
+    return characters;
+  }
+}
--- main.java
+++ main.java
@@ -1,25 +1,11 @@
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Vector;
-
-public class main {
+public class Main {
     public static void main(String[] args) {
         String fileName = "example.txt";
 
-        Vector<Character> characters = new Vector<>();
-
-        try (FileReader reader = new FileReader(fileName)) {
-            int c;
-            while ((c = reader.read()) != -1) {
-                characters.add((char) c);
-            }
-        } catch (IOException e) {
-            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        if (args.length > 0) {
+            fileName = args[0];
         }
 
-        System.out.println("Caracteres lidos do arquivo:");
-        for (Character ch : characters) {
-            System.out.print(ch);
-        }
+        ReadFileToVector.readFileToVector(fileName);
     }
 }
