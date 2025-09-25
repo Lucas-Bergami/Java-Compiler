f08fc98c836b82546fbca463e7cc9a77c608f2c0
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-09-25 12:23:49 -0300

    renamed some files and added lexical analyser file

---
+++ LexicalAnalyser.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+
+public class LexicalAnalyser {
+	private Vector<Token> tokens;
+
+	public Vector<Token> analyse(Vector<Character> characters){
+		
+		return tokens;
+	}
+}
